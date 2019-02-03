a82f01e041282f2c1d2631de8f19ef5157426c4c
Sascha Moecker <moeckersascha@hotmail.com>
2019-02-03 16:40:17 +0100

    Visualite single faces

--- browse.py
+++ browse.py
@@ -4,30 +4,43 @@ import argparse
 
 from tqdm import tqdm
 
-from visualization import visualize_faces
+from visualization import visualize_faces, visualize_face_in_files
 
+
+def find_faces(search_name, files_with_tags):
+    search_name = search_name
+    files_with_name = []
+    for file_with_tag in files_with_tags:
+        for tags in file_with_tag.get("faces"):
+            name = tags.get("name")
+            if search_name in name:
+                file_name = file_with_tag.get("file")
+                files_with_name.append({"name":name, "file":file_with_tag.get("file"), "face":tags})
+    return files_with_name
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--pickle_file', help='Pickle file (acts as database)',
                         default="files_with_tags.p")
+    parser.add_argument('--search_name', help='Name to search in database')
     parser.add_argument('--plot', help='Shall plot face tags?',
                         default="no")
 
     args = parser.parse_args()
+    shall_plot = True if args.plot == "yes" else False
 
     logging.info("Loading pickle file...")
     files_with_tags = pickle.load(open(args.pickle_file, "rb"))
     logging.info("Loadad {} file(s) with face tags".format(len(files_with_tags)))
 
-    shall_plot = True if args.plot == "yes" else False
-    visualize_faces(files_with_tags, shall_plot)
+    search_name = args.search_name
+    files_with_name = find_faces(search_name, files_with_tags)
+    logging.info("Found {} faces for {}".format(len(files_with_name), search_name))
 
-    for file_with_tag in tqdm(files_with_tags):
-        for tags in file_with_tag.get("faces"):
-            name = tags.get("name")
-            if name == "Jana Wenig":
-                print(tags.get("left"))
+
+    if shall_plot:
+        visualize_faces(files_with_tags, False)
+        visualize_face_in_files(files_with_name)
 
 
 if __name__ == "__main__":
--- visualization.py
+++ visualization.py
@@ -2,6 +2,9 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 import matplotlib.patches as patches
+import math
+from tqdm import tqdm
+import logging
 
 
 def visualize_face(file_path, face):
@@ -12,10 +15,27 @@ def visualize_face(file_path, face):
     left = int(face["left"])
     width = int(face["right"]-face["left"])
     img = img[top:top+height , left:left+width, :]
+    return img
 
-    fig, axis = plt.subplots(1)
-    imgplot = axis.imshow(img)
-    
+
+def visualize_face_in_files(files_with_name):
+    if len(files_with_name) == 0:
+        return
+    logging.info("Prepare plotting images...")
+
+    rows_cols = math.ceil(math.sqrt(len(files_with_name)))
+    rows = rows_cols
+    cols = rows_cols
+    fig, ax = plt.subplots(nrows=rows_cols, ncols=rows_cols)
+
+    logging.info("Ploting images...")
+    for i, file_with_name in enumerate(tqdm(files_with_name)):
+        file_path = file_with_name.get("file")
+        face = file_with_name.get("face")
+        img = visualize_face(file_path, face)
+
+        plt.imshow(img, aspect="auto")
+        fig.add_subplot(rows, cols, i+1)
     plt.show()
 
 
@@ -37,6 +57,6 @@ def visualize(file_path, faces):
 def visualize_faces(files_with_tags, plot=False):
     for file_with_tag in files_with_tags:
         list_of_name = [faces.get("name") for faces in file_with_tag.get("faces")]
-        print("File {} which has faces {}".format(file_with_tag.get("file"), list_of_name))
+        logging.debug("File {} which has faces {}".format(file_with_tag.get("file"), list_of_name))
         if plot:
             visualize(file_with_tag.get("file"), file_with_tag.get("faces"))
