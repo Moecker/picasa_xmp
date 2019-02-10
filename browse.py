78cca1aa8212637e440abdaa0c82a1243f78864d
Sascha Moecker <moeckersascha@hotmail.com>
2019-02-10 16:17:35 +0100

    Add stats to browsw

--- browse.py
+++ browse.py
@@ -7,10 +7,23 @@ from tqdm import tqdm
 from visualization import visualize_faces, visualize_face_in_files
 
 
+def make_face_to_file_dict(files_with_tags):
+    faces_to_filetags_dict = {}
+    for file_with_tag in files_with_tags:
+        for tags in file_with_tag.get("faces"):
+            name = tags.get("name")
+            if name not in faces_to_filetags_dict:
+                faces_to_filetags_dict[name] = []
+            else:
+                file_name = file_with_tag.get("file")
+                faces_to_filetags_dict[name].append({"file":file_with_tag.get("file"), "face":tags})
+    return faces_to_filetags_dict
+
+
 def find_faces(search_name, files_with_tags):
-    search_name = search_name
     files_with_name = []
     for file_with_tag in files_with_tags:
+        names_in_file = {}
         for tags in file_with_tag.get("faces"):
             name = tags.get("name")
             if search_name in name:
@@ -18,11 +31,12 @@ def find_faces(search_name, files_with_tags):
                 files_with_name.append({"name":name, "file":file_with_tag.get("file"), "face":tags})
     return files_with_name
 
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--pickle_file', help='Pickle file (acts as database)',
                         default="files_with_tags.p")
-    parser.add_argument('--search_name', help='Name to search in database')
+    parser.add_argument('--search_name', help='Name to search in database', default="")
     parser.add_argument('--plot', help='Shall plot face tags?',
                         default="no")
 
@@ -35,8 +49,12 @@ def main():
 
     search_name = args.search_name
     files_with_name = find_faces(search_name, files_with_tags)
-    logging.info("Found {} faces for {}".format(len(files_with_name), search_name))
+    logging.info("Found {} face(s) for search '{}''".format(len(files_with_name), search_name))
 
+    faces_to_filetags_dict = make_face_to_file_dict(files_with_tags)
+    logging.info("Found {} different face(s)".format(len(faces_to_filetags_dict.keys())))
+    for key, value in sorted(faces_to_filetags_dict.items(), key=lambda kv: len(kv[1]), reverse=True):
+        print("{} appears in {} images".format(key, len(value)))
 
     if shall_plot:
         visualize_faces(files_with_tags, False)
